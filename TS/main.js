e8dcc3075337cd68bd65c2c91737d97e21da5314
Erik <anderson8221@student.cptc.edu>
2020-11-01 16:44:10 -0800

    it works
    also red

--- TS/main.js
+++ TS/main.js
@@ -8,19 +8,74 @@ window.onload = function () {
     addBtn.onclick = addEmployee;
 };
 function addEmployee() {
-    alert("hi");
+    clearErrors();
+    var errs = validate();
+    if (errs == 0) {
+        displayEmployee(getFormData());
+    }
+}
+function displayEmployee(emp) {
+    document.getElementById("display").innerText =
+        "Name: " + emp.Fname + " " + emp.Lname + "\n" +
+            "Employee ID number: " + emp.Id + "\n" +
+            "Date of Birth: " + (emp.DOB.getUTCMonth()+1) + "/" + emp.DOB.getUTCDate() + "/" +emp.DOB.getFullYear() + "\n" +
+            "Location: " + emp.Location;
+    //this is gross, never do this
 }
 function getFormData() {
     var emp = new Employee();
-    var FnameData = document.getElementById("Fname");
-    var LnameData = document.getElementById("Fname");
-    var IdData = document.getElementById("Fname");
-    var DOBData = document.getElementById("Fname");
-    var LocationData = document.getElementById("Fname");
+    var FnameData = getInputElement("fname");
+    var LnameData = getInputElement("lname");
+    var IdData = getInputElement("id");
+    var DOBData = getInputElement("dob");
+    var LocationData = getInputElement("location");
     emp.DOB = DOBData.valueAsDate;
     emp.Fname = FnameData.value;
     emp.Lname = LnameData.value;
     emp.Id = IdData.value;
     emp.Location = LocationData.value;
     return emp;
+}
+function validate() {
+    var err = 0;
+    if (getInputElement("fname").value == "" || getInputElement("lname").value == "") {
+        addError("Please enter a name");
+        err++;
+    }
+    var id = getInputElement("id").value;
+    if (id == "") {
+        addError("Please enter an Id");
+        err++;
+    }
+    else if (id.length != 8) {
+        addError("Please enter valid Id code");
+        err++;
+    }
+    if (getInputElement("dob").valueAsDate == null) {
+        addError("Please enter a date of birth");
+        err++;
+    }
+    else if (getInputElement("dob").valueAsDate.getTime >= Date.now) {
+        addError("Please enter a time in the past");
+        err++;
+    }
+    if (getInputElement("location").value == "Please select a location") {
+        addError("Please select a location");
+        err++;
+    }
+    return err;
+}
+function getInputElement(Id) {
+    return document.getElementById(Id);
+}
+function addError(error) {
+    var ul = document.getElementById("errorList");
+    var li = document.createElement("li");
+    li.innerText = error;
+    ul.appendChild(li);
+}
+function clearErrors() {
+    var ul = document.getElementById("errorList");
+    while (ul.firstChild)
+        ul.removeChild(ul.firstChild);
 }
--- TS/main.ts
+++ TS/main.ts
@@ -12,25 +12,30 @@ window.onload = function(){
 }
 
 function addEmployee():void{
-    let errList:string[] = validate();
-    if(errList.length > 0){
-        errList.forEach(err => {
-            let ul:HTMLUListElement = <HTMLUListElement>document.getElementById("errorList");
-            let li:HTMLLIElement = document.createElement("li")
-            li.innerText = err;
-            ul.appendChild(li)
-        });
+    clearErrors()
+    let errs:number = validate();   
+    if(errs == 0){
+        displayEmployee(getFormData())
     }
+}
+
+function displayEmployee(emp:Employee):void{
+    document.getElementById("display").innerText = 
+    "Name: " + emp.Fname + " " + emp.Lname + "\n" +
+    "Employee ID number: " + emp.Id + "\n" +
+    "Date of Birth: " + (emp.DOB.getUTCMonth()+1) + "/" + emp.DOB.getUTCDate() + "/" +emp.DOB.getFullYear() + "\n" +
+    "Location: " + emp.Location;
+    //this is gross, never do this
 }
 
 function getFormData():Employee{
     let emp = new Employee();
 
-    let FnameData = getInputElement("Fname");
-    let LnameData = getInputElement("Fname");
-    let IdData = getInputElement("Fname");
-    let DOBData = getInputElement("Fname");
-    let LocationData = getInputElement("Fname");
+    let FnameData = getInputElement("fname");
+    let LnameData = getInputElement("lname");
+    let IdData = getInputElement("id");
+    let DOBData = getInputElement("dob");
+    let LocationData = getInputElement("location");
 
     emp.DOB = DOBData.valueAsDate;
     emp.Fname = FnameData.value;
@@ -41,33 +46,52 @@ function getFormData():Employee{
     return emp;
 }
 
-function validate():string[]{
-    let errList:string[] = [];
+function validate():number{
+    let err:number = 0;
 
-    if((getInputElement("Fname")).value == "" || getInputElement("Lname").value == ""){
-        errList.concat("Please enter a name")
+    if(getInputElement("fname").value == "" || getInputElement("lname").value == ""){
+        addError("Please enter a name")
+        err++;
+        
     }
-    let id:string = getInputElement("employeeId").value;
-    if(id = ""){
-        errList.concat("Please enter an Id")
+    let id:string = getInputElement("id").value;
+    if(id == ""){
+        addError("Please enter an Id")
+        err++;
     }
-    else if(id.length != 9){
-        errList.concat("Invalid Id code")
+    else if(id.length != 8){
+        addError("Please enter valid Id code")
+        err++;
     }
-    if(getInputElement("DOB").valueAsDate == null){
-        errList.concat("Please enter a date of birth")
+    if(getInputElement("dob").valueAsDate == null){
+        addError("Please enter a date of birth")
+        err++;
     }
-    else if(getInputElement("DOB").valueAsDate.getTime >= Date.now){
-        errList.concat("Please enter a time in the past")
+    else if(getInputElement("dob").valueAsDate.getTime >= Date.now){
+        addError("Please enter a time in the past")
+        err++;
     }
 
     if(getInputElement("location").value == "Please select a location"){
-        errList.concat("Please select a location")
+        addError("Please select a location")
+        err++;
     }
-    return errList;
+    return err;
     
 }
 
 function getInputElement(Id:string):HTMLInputElement{
     return <HTMLInputElement>document.getElementById(Id)
+}
+
+function addError(error:string):void{
+    let ul:HTMLUListElement = <HTMLUListElement>document.getElementById("errorList");
+    let li:HTMLLIElement = document.createElement("li")
+    li.innerText = error;
+    ul.appendChild(li)
+}
+
+function clearErrors():void{
+    let ul:HTMLUListElement = <HTMLUListElement>document.getElementById("errorList");
+    while(ul.firstChild)ul.removeChild(ul.firstChild)
 }
